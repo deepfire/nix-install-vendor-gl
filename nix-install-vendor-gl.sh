c3a2be19b8e2ca3fcdfc074a5b5d8f777ce1d3f8
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-05-17 14:09:54 +0300

    Refactoring, better diagnostics

--- nix-install-vendor-gl.sh
+++ nix-install-vendor-gl.sh
@@ -7,8 +7,9 @@
 # To add a new driver stack, the following 6 functions need to be modified:
 #
 #  compute_system_vendorgl_kind,  compute_system_vendorgl_version,
-#  nix_vendorgl_attribute,  nix_vendorgl_version_get_attribute_name,
-#  nix_vendorgl_package_compute_url, nix_vendorgl_package_name
+#  nixpkgs_vendorgl_attribute,  nixpkgs_vendorgl_version_get_attribute_name,
+#  nixpkgs_vendorgl_package_id, nixpkgs_vendorgl_package_name,
+#  vendorgl_package_compute_url
 #
 
 explain() {
@@ -52,11 +53,14 @@ EOF
 ###
 default_operation='install-vendor-gl'
 run_opengl_driver='/run/opengl-driver'
+run_opengl_driver_drvref='/tmp/run-opengl-driver.drv'
 cachedir="${XDG_CACHE_DIR:-${HOME}/.cache}/nix-install-vendor-gl"
+fix_attempted=
+vendorgl_deriv_log=
 
 arg_system_glxinfo='/usr/bin/glxinfo'
 arg_nix_glxinfo=${HOME}'/.nix-profile/bin/glxinfo'
-arg_nix_vendorgl_attr=
+arg_nixpkgs_vendorgl_attr=
 arg_nixpkgs=
 arg_dump_and_exit=
 arg_verbose=
@@ -143,7 +147,7 @@ do
     case "$1"
     in
 	--nixpkgs )                       arg_nixpkgs=$2; shift;;
-	--nix-vendor-gl-attr )  arg_nix_vendorgl_attr=$2; shift;;
+	--nix-vendor-gl-attr )  arg_nixpkgs_vendorgl_attr=$2; shift;;
 	--nix-glxinfo )               arg_nix_glxinfo=$2; shift;;
 	--system-glxinfo )         arg_system_glxinfo=$2; shift;;
 	###
@@ -193,42 +197,52 @@ compute_system_vendorgl_version() {
 	        amd-mesa ) echo ${nix_mesagl_version};;
 	        nvidia   ) echo ${system_vendorgl_version_string} | cut -d ' ' -f3;; esac; }
 
-nix_opengl_state() {
-        local stderr="`${arg_nix_glxinfo} 2>&1 >/dev/null`"
-        if test -z "$stderr"
-        then echo "ok"
-        else echo "broken"; fi; }
+nix_glxinfo_libgl_path() {
+        ldd ${arg_nix_glxinfo} 2>/dev/null | grep 'libGL\.so\.1' | cut -d'>' -f2 | cut -d' ' -f2 | xargs realpath -e 2>/dev/null | xargs dirname || true; }
 
-nix_vendorgl_attribute() {
-	if test ! -z "${arg_nix_vendorgl_attr}"
-	then vendorgl_attribute=${arg_nix_vendorgl_attr}
+nix_opengl_state() {
+        local stderr="`${arg_nix_glxinfo} 2>&1 >/dev/null || true`"
+        if   test -n "$stderr"
+        then echo "broken"
+        elif test "$(nix_glxinfo_libgl_path)" = "$(realpath -e ${run_opengl_driver}/lib 2>/dev/null || true)";
+        then echo "ok, fixed"
+        elif true
+        then echo "ok, unknown reason"
+        fi; }
+
+nixpkgs_vendorgl_attribute() {
+	if test ! -z "${arg_nixpkgs_vendorgl_attr}"
+	then vendorgl_attribute=${arg_nixpkgs_vendorgl_attr}
 	else case ${vendorgl} in
 		     nvidia ) echo 'linuxPackages.nvidia_x11';; esac; fi; }
 
-nix_vendorgl_version_get_attribute_name() {
+nixpkgs_vendorgl_version_get_attribute_name() {
         case ${vendorgl} in
 		nvidia ) echo 'if builtins.hasAttr "version" '${vendorgl_attribute}' then '${vendorgl_attribute}'.version else '${vendorgl_attribute}'.versionNumber';; esac; }
 
-nix_vendorgl_package_compute_url() {
+vendorgl_package_compute_url() {
 	case ${vendorgl} in
 		nvidia ) echo "http://download.nvidia.com/XFree86/Linux-x86_64/${system_vendorgl_version}/NVIDIA-Linux-x86_64-${system_vendorgl_version}.run";; esac; }
 
-nix_vendorgl_package_name() {
+nixpkgs_vendorgl_package_id() {
+	case ${vendorgl} in
+		nvidia ) echo "nvidia-x11";; esac; }
+
+nixpkgs_vendorgl_package_name() {
 	case ${vendorgl} in
 		nvidia ) echo "nvidia-x11-${system_vendorgl_version}-\${pkgs.linuxPackages.kernel.version}";; esac; }
 
 ###
 ###
-nix_vendorgl_get_driver_version() {
-	nix_eval "`nix_vendorgl_version_get_attribute_name`"; }
+nixpkgs_vendorgl_version() {
+	nix_eval "`nixpkgs_vendorgl_version_get_attribute_name`"; }
 
-system_vendorgl_matches_nix_vendorgl() {
-	test "${system_vendorgl_version}" = ${nix_vendorgl_driver_version} &&
+system_vendorgl_matches_nixpkgs_vendorgl() {
+	test "${system_vendorgl_version}" = ${nixpkgs_vendorgl_driver_version} &&
 		echo yes || echo no; }
 
 dump_internal_vars() {
      cat <<EOF
---------------------------------- Dumping internal variables:
 nix-build:                        ${nix_build}
 arg_nixpkgs:                      ${arg_nixpkgs}
 arg_system_glxinfo:               ${arg_system_glxinfo}
@@ -251,7 +265,58 @@ $(ldd ${system_libgl1_path})
 
 Default Nix GL vendor:            ${nix_vendorgl_client_string}
 Default Nix GL version:           ${nix_opengl_version_string}
-Default Nix GL broken:            $(nix_opengl_state)
+Nix GL state:                     $(nix_opengl_state)
+EOF
+}
+
+examine() {
+        local state="$(nix_opengl_state)"
+	dump_internal_vars
+	cat <<EOF
+--------------------------------- General system info:
+$(lsb_release -a 2>&1)
+--------------------------------- Nix 'glxinfo' linkage:
+$(ldd ${arg_nix_glxinfo} || true)
+--------------------------------- ${run_opengl_driver}/lib:
+EOF
+        ls -l ${run_opengl_driver}/lib || true
+        ls ${run_opengl_driver}/lib/ || true
+	cat <<EOF
+Has libGL.so.1:                   $(realpath -e ${run_opengl_driver}/lib/libGL.so.1 2>&1 || true)
+--------------------------------- 'glxinfo' stderr:
+EOF
+	${arg_nix_glxinfo} 2>&1 >/dev/null || true
+        if test -n "${fix_attempted}"
+        then
+                cat <<EOF
+--------------------------------- Attempted fix traces:
+Vendor GL derivation:             ${vendorgl_deriv}
+Date:                             $(date)
+Vendor GL log:                    ${vendorgl_deriv_log}
+EOF
+                bzcat ${vendorgl_deriv_log}
+        fi
+        cat <<EOF
+--------------------------------- System GL:
+System kernel version		  ${system_kernel_version}
+System GL vendor string:	  ${system_vendorgl_client_string}
+System GL vendor kind:		  ${vendorgl}
+System GL vendor driver version:  ${system_vendorgl_version}
+Vendor GL Nix attribute:	  ${vendorgl_attribute}
+Vendor GL package URL:		  $(vendorgl_package_compute_url)
+Vengor GL Nix package name:	  $(nixpkgs_vendorgl_package_name)
+--------------------------------- Nix:
+Nix kernel version:		  ${nix_kernel_version}
+Nix version:			  $(nix-env --version)
+Nixpkgs:			  ${arg_nixpkgs}
+Nixpkgs ver:			  $(nix_eval lib.nixpkgsVersion)
+Nix default GL driver version:	  ${nixpkgs_vendorgl_driver_version}
+Nix glxinfo libGL path:           $(nix_glxinfo_libgl_path)
+${run_opengl_driver}/lib is:        $(realpath -e ${run_opengl_driver}/lib 2>/dev/null || true)
+LD_LIBRARY_PATH:                  $LD_LIBRARY_PATH
+---------------------------------
+Sys vend. GL = Nixpkgs vend. GL:  $(system_vendorgl_matches_nixpkgs_vendorgl)
+Nix GL state:                     ${state}
 EOF
 }
 
@@ -361,63 +426,40 @@ EOF
 
         ## The rest of code assumes valid values of ${vendorgl}.
         ##
-        vendorgl_attribute=`nix_vendorgl_attribute`
+        vendorgl_attribute=`nixpkgs_vendorgl_attribute`
 
-        nix_vendorgl_driver_version=`nix_vendorgl_get_driver_version`
-        if test -z "${nix_vendorgl_driver_version}"
+        nixpkgs_vendorgl_driver_version=`nixpkgs_vendorgl_version`
+        if test -z "${nixpkgs_vendorgl_driver_version}"
         then fail "Nix vendor GL attribute ${vendorgl_attribute} is wrong, please supply a better one using --nix-vendor-gl-attr"
         fi
 
-        nix_vendorgl_package_url="`nix_vendorgl_package_compute_url`"
+        vendorgl_package_url="`vendorgl_package_compute_url`"
 
 
         ### Main & toplevel command dispatch
         ###
         case ${operation} in
-        examine )
-	        dump_internal_vars
-	        cat <<EOF
---------------------------------- General system info:
-$(lsb_release -a 2>&1)
---------------------------------- System GL:
-System kernel version		  ${system_kernel_version}
-System GL vendor string:	  ${system_vendorgl_client_string}
-System GL vendor kind:		  ${vendorgl}
-System GL vendor driver version:  ${system_vendorgl_version}
-Vendor GL Nix attribute:	  ${vendorgl_attribute}
-Vendor GL package URL:		  $(nix_vendorgl_package_compute_url)
-Vengor GL Nix package name:	  $(nix_vendorgl_package_name)
---------------------------------- Nix:
-Nix kernel version:		  ${nix_kernel_version}
-Nix version:			  $(nix-env --version)
-Nixpkgs:			  ${arg_nixpkgs}
-Nixpkgs ver:			  $(nix_eval lib.nixpkgsVersion)
-Nix GL vendor driver version:	  ${nix_vendorgl_driver_version}
----------------------------------
-Sys vendor GL = Nix vendor GL:	  $(system_vendorgl_matches_nix_vendorgl)
-EOF
-	        ;;
-
+        examine ) examine;;
         install-vendor-gl )
 	        tmpnix=`mktemp`
-	        if test "`NIX_PATH=${NIX_PATH}:nixpkgs-overlays=/tmp/overlay; system_vendorgl_matches_nix_vendorgl`" != 'yes'
+	        if test "`NIX_PATH=${NIX_PATH}:nixpkgs-overlays=/tmp/overlay; system_vendorgl_matches_nixpkgs_vendorgl`" != 'yes'
 	        then
-	                info "The version of the vendor driver in nixpkgs:  ${nix_vendorgl_driver_version}\ndoesn't match the system vendor driver version:     ${system_vendorgl_version}\n..so a semi-automated vendor GL package installation is required.\n"
+	                info "The version of the vendor driver in nixpkgs:  ${nixpkgs_vendorgl_driver_version}\ndoesn't match the system vendor driver version:     ${system_vendorgl_version}\n..so a semi-automated vendor GL package installation is required.\n"
 	                vendorgl_package_sha256_file="${cachedir}/${system_vendorgl_version}"
 	                mkdir -p "${cachedir}"
 	                vendorgl_package_sha256_cached="$(test ! -f ${vendorgl_package_sha256_file} || cat ${vendorgl_package_sha256_file})"
-	                nix_vendorgl_package_sha256=`nix-prefetch-url --type sha256 ${nix_vendorgl_package_url} ${vendorgl_package_sha256_cached}`
-	                echo -n "${nix_vendorgl_package_sha256}" > "${vendorgl_package_sha256_file}"
+	                vendorgl_package_sha256=`nix-prefetch-url --type sha256 ${vendorgl_package_url} ${vendorgl_package_sha256_cached}`
+	                echo -n "${vendorgl_package_sha256}" > "${vendorgl_package_sha256_file}"
 	                cat >${tmpnix} <<EOF
 with import <nixpkgs> { config = { allowUnfree = true; }; };
 let vendorgl = (${vendorgl_attribute}.override {
       libsOnly = true;
       kernel   = null;
     }).overrideAttrs (oldAttrs: rec {
-      name = "$(nix_vendorgl_package_name)";
+      name = "$(nixpkgs_vendorgl_package_name)";
       src = fetchurl {
-	url = "${nix_vendorgl_package_url}";
-	sha256 = "${nix_vendorgl_package_sha256}";
+	url = "${vendorgl_package_url}";
+	sha256 = "${vendorgl_package_sha256}";
       };
       useGLVND = 0;
     });
@@ -438,13 +480,24 @@ EOF
 	        echo; echo "Installing the vendor driver into: ${run_opengl_driver}"
 	        nix_build_options='--no-build-output --max-jobs 4 --cores 0'
 	        # NIX_PATH=nixpkgs=${arg_nixpkgs} ${nix_build} ${nix_build_options} ${tmpnix} ${arg_verbose:+-v}
-	        sudo NIX_PATH=nixpkgs=${arg_nixpkgs} ${nix_build} ${nix_build_options} ${tmpnix} ${arg_verbose:+-v} -o ${run_opengl_driver}
+	        sudo NIX_PATH=nixpkgs=${arg_nixpkgs} ${nix_build} ${nix_build_options} ${tmpnix} ${arg_verbose:+-v} \
+                     --out-link ${run_opengl_driver} \
+                     --drv-link ${run_opengl_driver_drvref}
 	        rm -f ${tmpnix}
+                run_opengl_driver_drv=`realpath -e ${run_opengl_driver_drvref} || true`
+                vendorgl_deriv=`nix-store --query --references ${run_opengl_driver_drv} | grep $(nixpkgs_vendorgl_package_id)`
+                vendorgl_deriv_hash=`echo ${vendorgl_deriv} | cut -c12- | cut -d- -f1`
+                vendorgl_deriv_log=`realpath /nix/var/log/nix/drvs/$(echo ${vendorgl_deriv_hash} | cut -c-2)/$(echo ${vendorgl_deriv_hash} | cut -c3-)-$(nixpkgs_vendorgl_package_id)-*`
+                echo
+                echo "GL driver derivation:      ${run_opengl_driver_drv}"
+                echo "Vendor driver derivation:  ${vendorgl_deriv}"
+                echo "Vendor driver build log:   ${vendorgl_deriv_log}"
 
 		export LD_LIBRARY_PATH=${run_opengl_driver}/lib
+                fix_attempted=true
 
 	        case $(nix_opengl_state) in
-	                ok ) cat <<EOF
+	                "ok"* ) cat <<EOF
 
 Nix-compatible vendor GL driver is now installed at ${run_opengl_driver}
 
@@ -470,12 +523,8 @@ as a bug report to:
    https://github.com/deepfire/nix-install-vendor-gl/issues
 
 EOF
-		export LD_LIBRARY_PATH=${run_opengl_driver}/lib
 		echo '--------------------------- 8< ---------------------------'
-		ldd ${arg_nix_glxinfo} || true
-                ls -l ${run_opengl_driver}
-                ls ${run_opengl_driver}/lib/
-		${arg_nix_glxinfo} 2>&1 >/dev/null || true
+                examine
 		echo '--------------------------- 8< ---------------------------'
 		cat <<EOF
 
